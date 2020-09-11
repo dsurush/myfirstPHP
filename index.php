b47acb772162f3aa1634d64e314584efbfabed71
dsurush <davlatzoda.surush@gmail.com>
2020-09-11 16:17:02 +0500

    function with address

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,44 +9,59 @@
 <body>
 <?php
 
-function power ($a){
-    return $a * $a;
-}
-function error_msg ($err_str)
-{
-    echo "<b>0шибка!<br>Причина: Bы ввели отрицательное число</b>";
-}
-error_msg("Bы ввели отрицательное число");
-echo "<br> Ответ:".power(2);
-echo "<br>";
-$phones = array("apple"=>"iPhone5",
-    "samsumg"=>"Samsung Galaxy III",
-    "nokia" => "Nokia N9",
-    "sony" => "Sony XPeria Z3");
+    function power ($a){
+        return $a * $a;
+    }
+    function error_msg ($err_str)
+    {
+        echo "<b>0шибка!<br>Причина: Bы ввели отрицательное число</b>";
+    }
+    error_msg("Bы ввели отрицательное число");
+    echo "<br> Ответ:".power(2);
+    echo "<br>";
+    $phones = array("apple"=>"iPhone5",
+        "samsumg"=>"Samsung Galaxy III",
+        "nokia" => "Nokia N9",
+        "sony" => "Sony XPeria Z3");
 
-foreach($phones as $key=>$value)
-    echo "$value <br />";
+    foreach($phones as $key=>$value)
+        echo "$value <br />";
 
-$tablets = array("lenovo" => "Lenovo IdeaTab A3500",
-    "samsung" => "Samsung Galaxy Tab 4",
-    "apple" => "Apple iPad Air");
-asort($tablets);
+    $tablets = array("lenovo" => "Lenovo IdeaTab A3500",
+        "samsung" => "Samsung Galaxy Tab 4",
+        "apple" => "Apple iPad Air");
+    asort($tablets);
 
-echo "<ul>";
-foreach ($tablets as $key => $value)
-{
-    echo "<li>$key : $value</li>";
-}
-echo "</ul><br>";
-$login = "Не известно";
-$password = "Не известно";
-if (isset($_POST['login'])) $login = $_POST['login'];
-if (isset($_POST['password'])) $password = $_POST['password'];
+    echo "<ul>";
+    foreach ($tablets as $key => $value)
+    {
+        echo "<li>$key : $value</li>";
+    }
+    echo "</ul><br>";
+    $login = "Не известно";
+    $password = "Не известно";
+    if (isset($_POST['login'])) $login = $_POST['login'];
+    if (isset($_POST['password'])) $password = $_POST['password'];
 
-echo "Ваш логин: $login  <br> Ваш пароль: $password";
+    echo "Ваш логин: $login  <br> Ваш пароль: $password";
 
-require "form.php";
+    require "form.php";
 
+    $arr = [1, 2, 3, 4, 5];
+    print_r($arr);
+    echo "<br>";
+    print_r(array_reverse($arr));
+    $timestampt = time();
+    echo "<br>";
+    echo $timestampt;
+
+    echo "<br>";
+    function Upper(&$number){
+        $number++;
+        echo $number;
+    }
+    $number = 10;
+    Upper($number);
 ?>
 
 </body>
